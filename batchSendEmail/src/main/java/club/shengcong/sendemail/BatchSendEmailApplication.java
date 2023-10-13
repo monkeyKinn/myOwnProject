6b371bd1e19ef64cb0f33fc62f8a2b574a8dc58b
JinShengcong <752724895@qq.com>
2023-10-13 15:41:09 +0800

    整合apollo

--- batchSendEmail/src/main/java/club/shengcong/sendemail/BatchSendEmailApplication.java
+++ batchSendEmail/src/main/java/club/shengcong/sendemail/BatchSendEmailApplication.java
@@ -1,9 +1,11 @@
 package club.shengcong.sendemail;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableApolloConfig
 public class BatchSendEmailApplication {
 
     public static void main(String[] args) {
